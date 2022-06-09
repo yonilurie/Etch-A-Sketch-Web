fb291e478fce366564597670c083b810078a756e
yonilurie <yonilurie@protonmail.com>
2022-06-08 20:55:21 -0700

    added basic functionality

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 window.onload = () => {
-createGrid(size);
-}
+	createGrid(size);
+};
 
 const etchContainer = document.querySelector(".etch-container");
-etchContainer.style.gridTemplateRow = "repeat(10, 1fr)"
-etchContainer.style.gridTemplateColumns = "repeat(10, 1fr)"
-let size = 10;
+let size = 15;
+etchContainer.style.gridTemplateRow = `repeat(${size}, 1fr)`;
+etchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+etchContainer.addEventListener("mouseover", activated);
 
 function createGrid(size) {
 	for (let i = 0; i < size; i++) {
 		for (let j = 0; j < size; j++) {
-            const cell = document.createElement("div");
-            cell.setAttribute("class","cell")
+			const cell = document.createElement("div");
+			cell.setAttribute("class", "cell");
 			etchContainer.appendChild(cell);
 		}
 	}
+}
+
+function activated(event) {
+	event.target.setAttribute("class", "cell activated")
 }
