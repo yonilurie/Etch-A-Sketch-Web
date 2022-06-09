3b6d197c154ae6d630332dca63f3de78b891097c
yonilurie <yonilurie@protonmail.com>
2022-06-08 21:08:41 -0700

    Added color picker

--- js/index.js
+++ js/index.js
@@ -1,23 +1,36 @@
 window.onload = () => {
+	//Creates grid and appends to container on window load
 	createGrid(size);
 };
 
+const colorSelector = document.querySelector("input");
 const etchContainer = document.querySelector(".etch-container");
-let size = 15;
-etchContainer.style.gridTemplateRow = `repeat(${size}, 1fr)`;
-etchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-etchContainer.addEventListener("mouseover", activated);
+let currentColor = colorSelector.value;
+let size = 10;
 
-function createGrid(size) {
-	for (let i = 0; i < size; i++) {
-		for (let j = 0; j < size; j++) {
-			const cell = document.createElement("div");
-			cell.setAttribute("class", "cell");
-			etchContainer.appendChild(cell);
-		}
-	}
+//changes current color whenever the colorpicker value is changed
+colorSelector.addEventListener("change", (event) => {
+	currentColor = event.target.value;
+	console.log(currentColor);
+});
+
+// callback for event listener that sets background color of cells
+function activated(event) {
+	event.target.style.backgroundColor = currentColor;
 }
 
-function activated(event) {
-	event.target.setAttribute("class", "cell activated")
+function createGrid() {
+	for (let i = 0; i < size * size; i++) {
+		const cell = document.createElement("div");
+		cell.setAttribute("class", "cell");
+		etchContainer.appendChild(cell);
+	}
+	etchContainerDimensions();
+}
+
+//Sets dimension of etch container
+function etchContainerDimensions() {
+	etchContainer.style.gridTemplateRow = `repeat(${size}, 1fr)`;
+	etchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	etchContainer.addEventListener("mouseover", activated);
 }
