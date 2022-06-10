c4aed544b7ae95f477233780049101c7c2865e3f
yonilurie <yonilurie@protonmail.com>
2022-06-10 16:09:49 -0700

    added documentation and cleaned up

--- js/index.js
+++ js/index.js
@@ -4,16 +4,14 @@ window.addEventListener("DOMContentLoaded", () => {
 		if (e.code === "KeyP") togglePause();
 	});
 });
-
+// All DOM elements and variables
 const colorSelector = document.querySelector("input");
 const eraserBtn = document.getElementById("eraser-btn");
-
 const pause = document.getElementById("pause");
 const rainbowBtn = document.getElementById("rainbow-btn");
 const resetBtn = document.getElementById("reset-btn");
 const sizeSlider = document.getElementById("size-slider");
 const sizeText = document.getElementById("size-text");
-
 let etchContainer = document.querySelector(".etch-container");
 let eraser = false;
 let rainbow = false;
@@ -21,6 +19,7 @@ let currentColor = colorSelector.value;
 let size = sizeSlider.value;
 sizeText.innerText = size;
 
+//Initial event listeners added to DOM elements
 colorSelector.addEventListener("change", changeColor);
 eraserBtn.addEventListener("click", toggleEraser);
 rainbowBtn.addEventListener("click", toggleRainbow);
@@ -30,48 +29,51 @@ sizeSlider.addEventListener("input", (e) => {
 	sizeText.innerText = e.target.value;
 });
 
+//Functions
+//Colors in cell on mouseover with current color in color variable
 function activate(event) {
 	event.target.style.backgroundColor = currentColor;
 }
 
+//Colors in cell on mouseover with randomly generated number
 function activateRainbow(event) {
 	event.target.style.backgroundColor = generateRGB();
 }
 
+//When the user changes the value of the color input
+// this will change the color variable
 function changeColor(e) {
 	currentColor = e.target.value;
 	if (eraser) toggleEraser();
 	if (rainbow) toggleRainbow();
 }
 
+//When the user resizes the grid using the range input,
+//creates new grid and replaces old grid
+//The size variable is also updated
 function changeGridSize(e) {
 	size = e.target.value;
 	resetGrid();
 	if (eraser) toggleEraser();
 }
 
-//generates grid with dimensions of size by size, and appends to etch-container
+//Dynamically creates grid with size of 'size * size'
+//and adds event listener and grid styling
 function createGrid() {
 	for (let i = 0; i < size * size; i++) {
 		const cell = document.createElement("div");
 		cell.setAttribute("class", "cell");
 		etchContainer.appendChild(cell);
 	}
-	etchContainerDimensions();
-}
-
-//Sets dimension of etch container
-function etchContainerDimensions() {
 	etchContainer.style.gridTemplateRow = `repeat(${size}, 1fr)`;
 	etchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 	etchContainer.addEventListener("mouseover", activate);
 	if (rainbow) {
-		etchContainer.removeEventListener("mouseover", activate);
-		etchContainer.addEventListener("mouseover", activateRainbow);
+		removeAddMouseOverEventListener(activate, activateRainbow);
 	}
 }
 
-//Creates a random rgb value for the rainbow color option
+//Creates and returns a random rgb value for the rainbow color option
 function generateRGB() {
 	let RGBCode = [];
 	for (let i = 0; i < 3; i++) {
@@ -81,6 +83,9 @@ function generateRGB() {
 	return "rgb(" + RGBCode.join(",") + ")";
 }
 
+//callback function for pause event listener
+//toggles cursor changing grid cells
+//also reveals pause icon in options tab when active
 function pauseRemoveListeners(attribute, cb) {
 	if (etchContainer.getAttribute(`${attribute}`)) {
 		etchContainer.removeEventListener("mouseover", cb);
@@ -93,7 +98,14 @@ function pauseRemoveListeners(attribute, cb) {
 	}
 }
 
-//deletes old grid and create new one
+//removes one event listener and adds another event listener
+//from an element
+function removeAddMouseOverEventListener(cb1, cb2) {
+	etchContainer.removeEventListener("mouseover", cb1);
+	etchContainer.addEventListener("mouseover", cb2);
+}
+
+//deletes old grid and create new empty one
 function resetGrid() {
 	etchContainer.remove();
 	let newGridContainer = document.createElement("div");
@@ -118,6 +130,7 @@ function toggleEraser() {
 	}
 }
 
+//toggles the pause
 function togglePause() {
 	switch (rainbow) {
 		case true:
@@ -132,21 +145,16 @@ function togglePause() {
 
 //toggles the rainbow
 function toggleRainbow() {
-	//this runs if the rainbow is off
 	if (!rainbow) {
-		etchContainer.removeEventListener("mouseover", activate);
-		etchContainer.addEventListener("mouseover", activateRainbow);
-		rainbowBtn.setAttribute("class", "active-rainbow");
-		rainbow = true;
-		etchContainer.setAttribute("rainbow", "true");
-		//when turning on the rainbow, the eraser is turned off
 		if (eraser) toggleEraser();
-		//this runs if the rainbow is on
+		rainbow = true;
+		removeAddMouseOverEventListener(activate, activateRainbow);
+		etchContainer.setAttribute("rainbow", "true");
+		rainbowBtn.setAttribute("class", "active-rainbow");
 	} else {
-		etchContainer.removeEventListener("mouseover", activateRainbow);
-		etchContainer.addEventListener("mouseover", activate);
-		rainbowBtn.removeAttribute("class", "active-rainbow");
 		rainbow = false;
+		removeAddMouseOverEventListener(activateRainbow, activate);
 		etchContainer.removeAttribute("rainbow", "true");
+		rainbowBtn.removeAttribute("class", "active-rainbow");
 	}
 }
